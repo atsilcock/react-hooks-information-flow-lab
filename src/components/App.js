8e83961df1060ffd60f17eca75c07a10e690e134
atsilcock <143049271+atsilcock@users.noreply.github.com>
2024-07-29 11:55:06 -0600

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header.js"
+
+
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+
 
   function onDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -11,13 +16,9 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
+      <Header onDarkModeClick={onDarkModeClick} isDarkMode={isDarkMode}/>
+      
+      <ShoppingList  items={itemData} />
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Item from "./Item";
+import Filter from "./Filter"
+
 
 function ShoppingList({ items }) {
+
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
@@ -14,22 +17,17 @@ function ShoppingList({ items }) {
     return item.category === selectedCategory;
   });
 
+
   return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+  <div>  
+    <Filter onCategoryChange={handleCategoryChange}/>
+
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
-    </div>
+  </div>
   );
 }
 
